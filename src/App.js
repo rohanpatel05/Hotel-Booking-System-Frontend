ccfa2c53e4fad1622232799c15934aff01871ec4
Rohan Patel <rohangckp@gmail.com>
2024-04-13 19:40:14 -0500

    Added Not found page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home.js";
+import NotFound from "./pages/NotFound.js";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" Component={Home} />
+        <Route component={NotFound} />
       </Routes>
     </Router>
   );
