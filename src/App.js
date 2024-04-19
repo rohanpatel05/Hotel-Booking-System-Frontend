25faf1d8f9deaa15e9c407561eee1bc147203482
Rohan Patel <rohangckp@gmail.com>
2024-04-19 15:53:56 -0500

    Adding global layout.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { Layout } from "./components/Layout.js";
+
 import Home from "./pages/Home.js";
 import NotFound from "./pages/NotFound.js";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" Component={Home} />
-        <Route component={NotFound} />
-      </Routes>
-    </Router>
+    <React.Fragment>
+      <Layout>
+        <Router>
+          <Routes>
+            <Route exact path="/" Component={Home} />
+            <Route component={NotFound} />
+          </Routes>
+        </Router>
+      </Layout>
+    </React.Fragment>
   );
 }
 
