d9bba7ed9191ac1d1e95d18b650b7e0b1eff718c
Rohan Patel <rohangckp@gmail.com>
2024-04-19 16:41:16 -0500

    Defining styles for different pages' background.

---
+++ src/pages/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const NavyBackgroundPageWrapper = styled.div`
+  background-color: #395165;
+  min-height: 100vh;
+`;
+
+export const LBeigeBackgroundPageWrapper = styled.div`
+  background-color: #f4f1ed;
+  min-height: 100vh;
+`;
+
+export const DBeigeBackgroundPageWrapper = styled.div`
+  background-color: #efebe5;
+  min-height: 100vh;
+`;
