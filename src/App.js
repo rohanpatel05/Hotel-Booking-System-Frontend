ccc773d20a68a3cc2c6607b8d40a8ca1b8cedbe7
Rohan Patel <rohangckp@gmail.com>
2024-04-19 19:19:09 -0500

    Updated import path.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home.js";
+import Home from "./pages/Home/index.js";
 import NotFound from "./pages/NotFound.js";
 
 function App() {
