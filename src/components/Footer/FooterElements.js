8ff3288c4e8f3e70ef8eae8a8a890183bed3da0c
Rohan Patel <rohangckp@gmail.com>
2024-04-20 17:19:01 -0500

    Developed footer.

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -1,1 +1,26 @@
 import styled from "styled-components";
+import colors from "../../config/colors";
+
+export const Title = styled.h1`
+  text-align: center;
+  font-family: Italianno;
+  font-size: 50px;
+  padding: 35px 10px;
+  padding-bottom: 60px;
+  color: ${colors.white};
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const StyledHr = styled.hr`
+  border-top: 2px solid ${colors.white};
+  width: 100%;
+  margin-top: 10px;
+  margin-bottom: 60px;
+`;
+
+export const AlignedText = styled.div`
+  text-align: ${(props) => props.align};
+  font-family: "Inter", sans-serif;
+  font-size: 15px;
+  color: ${colors.pale};
+`;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,48 @@
 import React from "react";
 import { NavyBackgroundFooterWrapper } from "../../config/styles";
+import { Title, StyledHr, AlignedText } from "./FooterElements";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  COMPANY_ADDRESS,
+  COMPANY_EMAIL,
+  COMPANY_PHONE_NUMBER,
+  COMPANY_FACEBOOK,
+  COMPANY_INSTAGRAM,
+  COMPANY_POLICY,
+} from "../../config/textDescriptions";
 
 function Footer() {
-  return <NavyBackgroundFooterWrapper>Footer</NavyBackgroundFooterWrapper>;
+  return (
+    <NavyBackgroundFooterWrapper>
+      <Title>The Hotel</Title>
+      <Container>
+        <Row>
+          <Col>
+            <AlignedText align="left">{COMPANY_ADDRESS}</AlignedText>
+          </Col>
+          <Col>
+            <AlignedText align="center">{COMPANY_EMAIL}</AlignedText>
+          </Col>
+          <Col>
+            <AlignedText align="right">{COMPANY_PHONE_NUMBER}</AlignedText>
+          </Col>
+        </Row>
+        <StyledHr />
+        <Row>
+          <Col>
+            <AlignedText align="left">{COMPANY_FACEBOOK}</AlignedText>
+          </Col>
+          <Col>
+            <AlignedText align="center">{COMPANY_INSTAGRAM}</AlignedText>
+          </Col>
+          <Col>
+            <AlignedText align="right">{COMPANY_POLICY}</AlignedText>
+          </Col>
+        </Row>
+        <StyledHr />
+      </Container>
+    </NavyBackgroundFooterWrapper>
+  );
 }
 
 export default Footer;
