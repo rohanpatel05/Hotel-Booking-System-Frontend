4e1dc2bd9440b5aedc5d6264c52665ef57ee61cb
Rohan Patel <rohangckp@gmail.com>
2024-04-20 18:16:34 -0500

    Created the accommodations page.

---
+++ src/pages/Accommodations/AccommodationsElements.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  padding: 30px 10px;
+  font-family: KiwiMaruRegular;
+  font-size: 60px;
+  text-align: center;
+`;
---
+++ src/pages/Accommodations/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { DBeigeBackgroundPageWrapper } from "../../config/styles";
+import { Title } from "./AccommodationsElements";
+function Accommodations() {
+  return (
+    <DBeigeBackgroundPageWrapper>
+      <Title>Accommodations</Title>
+    </DBeigeBackgroundPageWrapper>
+  );
+}
+
+export default Accommodations;
