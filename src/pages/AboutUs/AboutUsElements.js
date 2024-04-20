4b2a4c8d9affcd8d579acd850597e94a722ebca7
Rohan Patel <rohangckp@gmail.com>
2024-04-20 15:33:23 -0500

    Developed about us page.

--- src/pages/AboutUs/AboutUsElements.js
+++ src/pages/AboutUs/AboutUsElements.js
@@ -1,1 +1,46 @@
 import styled from "styled-components";
+import { Card, CardText, CardImg } from "react-bootstrap";
+import colors from "../../config/colors";
+
+export const Title = styled.h1`
+  padding: 30px 10px;
+  font-family: KiwiMaruRegular;
+  font-size: 60px;
+  text-align: center;
+`;
+
+export const BodyWrapper = styled.div`
+  padding: 10px 40px;
+`;
+
+export const AboutUsTextCard = styled(Card)`
+  background-color: ${colors.darkBeige};
+  border-color: ${colors.darkBeige};
+`;
+
+export const AboutUsText = styled(CardText)`
+  font-family: KiwiMaruRegular;
+  font-size: 34px;
+`;
+export const ResortImageCard = styled(Card)`
+  background-color: ${colors.darkBeige};
+  border-color: ${colors.darkBeige};
+`;
+
+export const ResortImage = styled(CardImg)`
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+
+  margin: 0 auto;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  display: block;
+  transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+    opacity: 0.9;
+  }
+`;
--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { DBeigeBackgroundPageWrapper } from "../../config/styles";
+import {
+  Title,
+  BodyWrapper,
+  AboutUsTextCard,
+  AboutUsText,
+  ResortImageCard,
+  ResortImage,
+} from "./AboutUsElements";
+import { CardBody } from "react-bootstrap";
+import { ABOUT_US } from "../../config/textDescriptions";
+import ResortPhoto from "../../assets/images/ResortSquaredPlaceholder.png";
+import { Container, Row, Col } from "react-bootstrap";
 
 function AboutUs() {
-  return <div>About Us</div>;
+  return (
+    <DBeigeBackgroundPageWrapper>
+      <Title>About Us</Title>
+      <BodyWrapper>
+        <Container fluid>
+          <Row>
+            <Col md={6}>
+              <AboutUsTextCard>
+                <CardBody>
+                  <AboutUsText>{ABOUT_US}</AboutUsText>
+                </CardBody>
+              </AboutUsTextCard>
+            </Col>
+            <Col md={6}>
+              <ResortImageCard>
+                <CardBody>
+                  <ResortImage src={ResortPhoto} />
+                </CardBody>
+              </ResortImageCard>
+            </Col>
+          </Row>
+        </Container>
+      </BodyWrapper>
+    </DBeigeBackgroundPageWrapper>
+  );
 }
 
 export default AboutUs;
