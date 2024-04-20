b659fd3e5f063a77f97adebf55a603200c309770
Rohan Patel <rohangckp@gmail.com>
2024-04-20 16:20:31 -0500

    Created footer component.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { NavyBackgroundFooterWrapper } from "../../config/styles";
+
+function Footer() {
+  return <NavyBackgroundFooterWrapper>Footer</NavyBackgroundFooterWrapper>;
+}
+
+export default Footer;
