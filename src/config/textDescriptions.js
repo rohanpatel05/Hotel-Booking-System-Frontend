53bd644f6e8f9ab796688fc3756bcc205d6e29a8
Rohan Patel <rohangckp@gmail.com>
2024-04-20 11:26:50 -0500

    Added guest reviews structure.

--- src/config/textDescriptions.js
+++ src/config/textDescriptions.js
@@ -1,2 +1,23 @@
 export const WELCOMING_AD =
   "Relex and Unwind \nYour Cozy Retreat \nFrom Everyday Life";
+
+export const REVIEWS = [
+  {
+    id: 1,
+    review:
+      "Impeccably clean rooms, friendly staff, and top-notch amenities. Highly recommended for a delightful stay!",
+    guestName: "Sarah Thompson",
+  },
+  {
+    id: 2,
+    review:
+      "Exceptional service from welcoming staff. Breathtaking ocean views and elegantly furnished rooms made for a memorable experience.",
+    guestName: "The Smith Family",
+  },
+  {
+    id: 3,
+    review:
+      "Tranquil atmosphere, rejuvenating spa, and cozy rooms. Minor Wi-Fi issue aside, it was a refreshing retreat.",
+    guestName: "Emily and John",
+  },
+];
