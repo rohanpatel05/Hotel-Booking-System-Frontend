1665f3d08bb7484fe8f0a7988cac98a0adfe2af4
Rohan Patel <rohangckp@gmail.com>
2024-04-20 12:31:49 -0500

    Added reviews.

--- src/pages/GuestReview/GuestReview.js
+++ src/pages/GuestReview/GuestReview.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
+import { Card, CardTitle, CardText } from "react-bootstrap";
+import colors from "../../config/colors";
 
 export const Title = styled.h1`
-  padding: 150px 10px;
+  padding: 130px 10px;
   font-family: KiwiMaruRegular;
   font-size: 60px;
   text-align: center;
@@ -9,8 +11,28 @@ export const Title = styled.h1`
 
 export const ReviewCardsWrapper = styled.div`
   display: flex;
+  justify-content: space-between;
+  padding: 10px 40px;
 `;
 
-export const ReviewCards = styled.div`
-  padding: 10px;
+export const ReviewCards = styled(Card)`
+  width: 30%;
+
+  background-color: ${colors.lightBeige};
+  border-color: ${colors.lightBeige};
+`;
+
+export const RevieHeader = styled(CardTitle)`
+  font-family: KiwiMaruRegular;
+  font-size: 55px;
+`;
+
+export const RevieBody = styled(CardText)`
+  font-family: KiwiMaruRegular;
+  font-size: 20px;
+`;
+
+export const RevieFooter = styled(CardText)`
+  font-family: KiwiMaruMedium;
+  font-size: 20px;
 `;
--- src/pages/GuestReview/index.js
+++ src/pages/GuestReview/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import { LBeigeBackgroundPageWrapper } from "../styles";
-import { Title, ReviewCardsWrapper, ReviewCards } from "./GuestReview";
+import {
+  Title,
+  ReviewCardsWrapper,
+  ReviewCards,
+  RevieHeader,
+  RevieBody,
+  RevieFooter,
+} from "./GuestReview";
+import { REVIEWS } from "../../config/textDescriptions";
 
 function GuestReview() {
   return (
     <LBeigeBackgroundPageWrapper>
       <Title>What our guests says</Title>
+      <ReviewCardsWrapper>
+        {REVIEWS &&
+          REVIEWS.map((review) => (
+            <ReviewCards>
+              <RevieHeader>“</RevieHeader>
+              <RevieBody>{review.review}</RevieBody>
+              <RevieFooter>{review.guestName}</RevieFooter>
+            </ReviewCards>
+          ))}
+      </ReviewCardsWrapper>
     </LBeigeBackgroundPageWrapper>
   );
 }
