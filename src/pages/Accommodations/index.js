edd6c2af434ffffe8c74ca89aaf4a2a2db8c57bd
Rohan Patel <rohangckp@gmail.com>
2024-04-21 15:50:46 -0500

    Used TanStack Query and handled loading and error state.

--- src/pages/Accommodations/index.js
+++ src/pages/Accommodations/index.js
@@ -2,8 +2,32 @@ import React from "react";
 import { DBeigeBackgroundPageWrapper } from "../../config/styles";
 import { Title, StyledContainer, StyledCol } from "./AccommodationsElements";
 import { Row, Col } from "react-bootstrap";
+import { useQuery } from "@tanstack/react-query";
+import { fetchRooms } from "../../services/roomService";
+import { GET_ROOMS_QUERY_KEY } from "../../config/queryKeys";
+import Spinner from "../../components/Spinner.js";
+import StyledErrorAlert from "../../components/Error.js";
 
 function Accommodations() {
+  const {
+    data: rooms,
+    isError,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: [GET_ROOMS_QUERY_KEY],
+    queryFn: fetchRooms,
+  });
+
+  if (isLoading) return <Spinner />;
+
+  if (isError)
+    return (
+      <StyledErrorAlert variant="danger">
+        Error loading rooms&apos; info: {error.message}
+      </StyledErrorAlert>
+    );
+
   return (
     <DBeigeBackgroundPageWrapper>
       <Title>Accommodations</Title>
@@ -15,6 +39,7 @@ function Accommodations() {
           <Col>hi</Col>
         </Row>
       </StyledContainer>
+      <div>Data: {JSON.stringify(rooms)}</div>
     </DBeigeBackgroundPageWrapper>
   );
 }
