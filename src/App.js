356ae81ef2895050b278e1218cb9eab53aaa13e5
Rohan Patel <rohangckp@gmail.com>
2024-04-21 13:45:25 -0500

    Created Query Client at the root of the React component tree.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Home from "./pages/Home.js";
 import NotFound from "./pages/NotFound.js";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      onError: (error) => {
+        if (error.response) {
+          const responseError =
+            error.response.data.message || "Unknown error occurred.";
+          error.message = responseError;
+        } else {
+          error.message = "Network error occurred!";
+        }
+        console.error("Error:", error.message);
+      },
+    },
+    mutations: {
+      onError: (error) => {
+        if (error.response) {
+          const responseError =
+            error.response.data.message || "Unknown error occurred.";
+          error.message = responseError;
+        } else {
+          error.message = "Network error occurred!";
+        }
+        console.error("Error:", error.message);
+      },
+    },
+  },
+});
+
 function App() {
   return (
-    <React.Fragment>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </React.Fragment>
+    <QueryClientProvider client={queryClient}>
+      <React.Fragment>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </React.Fragment>
+    </QueryClientProvider>
   );
 }
 
