d9d42ef97fec61a401e005c0330e250263b28550
Rohan Patel <rohangckp@gmail.com>
2024-04-21 15:24:09 -0500

    Added an overlayed spinner component.

---
+++ src/components/Spinner.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import colors from "../config/colors";
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1050;
+`;
+
+const Spinner = styled.div`
+  border: 5px solid rgba(255, 255, 255, 0.3);
+  border-top: 5px solid ${colors.blue};
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: ${spin} 1.5s linear infinite;
+`;
+
+function OverlayedSpinner() {
+  return (
+    <Overlay>
+      <Spinner />
+    </Overlay>
+  );
+}
+
+export default OverlayedSpinner;
