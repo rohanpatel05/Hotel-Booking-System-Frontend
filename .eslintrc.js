28f49874046554838e9201590d528ae7d7f319f1
Rohan Patel <rohangckp@gmail.com>
2024-04-21 11:29:53 -0500

    Added TanStack query linting configuration.

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@tanstack/query'],
+  extends: [
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    '@tanstack/query/exhaustive-deps': 'error',
+    // "@tanstack/query/no-deprecated-options": "error",
+    // "@tanstack/query/prefer-query-object-syntax": "error",
+    // "@tanstack/query/stable-query-client": "error",
+  },
+};
