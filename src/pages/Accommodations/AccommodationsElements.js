4385f82cc011290cec3ca38510ea64803da76c5f
Rohan Patel <rohangckp@gmail.com>
2024-04-20 19:44:19 -0500

    Added grid style.

--- src/pages/Accommodations/AccommodationsElements.js
+++ src/pages/Accommodations/AccommodationsElements.js
@@ -1,8 +1,19 @@
 import styled from "styled-components";
+import colors from "../../config/colors";
+import { Container, Col } from "react-bootstrap";
 
 export const Title = styled.h1`
   padding: 30px 10px;
   font-family: KiwiMaruRegular;
   font-size: 60px;
   text-align: center;
+`;
+
+export const StyledContainer = styled(Container)`
+  border: 1px solid ${colors.navy};
+  border-radius: 20px;
+`;
+
+export const StyledCol = styled(Col)`
+  border-bottom: 1px solid ${colors.navy};
 `;
--- src/pages/Accommodations/index.js
+++ src/pages/Accommodations/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { DBeigeBackgroundPageWrapper } from "../../config/styles";
-import { Title } from "./AccommodationsElements";
+import { Title, StyledContainer, StyledCol } from "./AccommodationsElements";
+import { Row, Col } from "react-bootstrap";
+
 function Accommodations() {
   return (
     <DBeigeBackgroundPageWrapper>
       <Title>Accommodations</Title>
+      <StyledContainer>
+        <Row>
+          <StyledCol>hi</StyledCol>
+        </Row>
+        <Row>
+          <Col>hi</Col>
+        </Row>
+      </StyledContainer>
     </DBeigeBackgroundPageWrapper>
   );
 }
