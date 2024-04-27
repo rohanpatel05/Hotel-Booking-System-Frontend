9b5860af52c3ebfcaba68a6d2b30f79e445720bd
Rohan Patel <rohangckp@gmail.com>
2024-04-26 19:23:19 -0500

    Added styled rooms catelog.

--- src/pages/Accommodations/AccommodationsElements.js
+++ src/pages/Accommodations/AccommodationsElements.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import colors from "../../config/colors";
-import { Container, Col } from "react-bootstrap";
 
 export const Title = styled.h1`
   padding: 30px 10px;
@@ -9,11 +8,93 @@ export const Title = styled.h1`
   text-align: center;
 `;
 
-export const StyledContainer = styled(Container)`
+export const StyledContainer = styled.div`
+  margin: 0px 30px;
   border: 1px solid ${colors.navy};
   border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
 `;
 
-export const StyledCol = styled(Col)`
-  border-bottom: 1px solid ${colors.navy};
+export const StyledRow = styled.div`
+  &:not(:last-child) {
+    border-bottom: 1px solid ${colors.navy};
+  }
+
+  &:hover {
+    background-color: ${colors.navy};
+  }
+`;
+
+export const RoomCard = styled.div`
+  display: flex;
+  align-items: center;
+  transition: background-color 0.3s ease;
+  cursor: pointer;
+  position: relative;
+
+  &:hover {
+    & > .text-wrapper > .room-title,
+    & > .text-wrapper > .room-description {
+      color: ${colors.white};
+    }
+
+    & > .room-image {
+      height: 135px;
+      opacity: 1;
+      // margin-right: 20px;
+      transform: translateX(0);
+    }
+
+    & > .arrow-icon {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+`;
+
+export const RoomImage = styled.img`
+  height: 0px;
+  width: auto;
+
+  object-fit: cover;
+  object-position: center;
+  transition: all 0.3s ease, width 0.3s ease;
+  opacity: 0;
+  margin-right: 0;
+  order: -1;
+`;
+
+export const TextWrapper = styled.div`
+  padding: 20px 40px 5px;
+  flex-grow: 1;
+  max-width: 50%;
+`;
+
+export const RoomTitle = styled.h3`
+  font-family: KiwiMaruMedium, sans-serif;
+  font-size: 25px;
+  color: ${colors.black};
+  margin: 0;
+  transition: color 0.3s ease;
+`;
+
+export const RoomDescription = styled.p`
+  font-family: KiwiMaruRegular, sans-serif;
+  font-size: 12px;
+  color: ${colors.black};
+  margin-top: 10px;
+`;
+
+export const ArrowIcon = styled.div`
+  background-color: ${colors.white};
+  padding: 0px 20px;
+  border-radius: 20px;
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+  transition: all 0.3s ease;
+  opacity: 0;
+  transform: translateX(20px);
 `;
--- src/pages/Accommodations/index.js
+++ src/pages/Accommodations/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { DBeigeBackgroundPageWrapper } from "../../config/styles";
-import { Title, StyledContainer, StyledCol } from "./AccommodationsElements";
-import { Row, Col } from "react-bootstrap";
+import {
+  Title,
+  StyledContainer,
+  StyledRow,
+  RoomCard,
+  RoomImage,
+  ArrowIcon,
+  TextWrapper,
+  RoomTitle,
+  RoomDescription,
+} from "./AccommodationsElements";
 import { useQuery } from "@tanstack/react-query";
 import { fetchRooms } from "../../services/roomService";
 import { GET_ROOMS_QUERY_KEY } from "../../config/queryKeys";
 import Spinner from "../../components/Spinner.js";
 import StyledErrorAlert from "../../components/Error.js";
+import RoomPhoto from "../../assets/images/RoomPlaceholder1.png";
 
 function Accommodations() {
   const {
@@ -32,14 +42,25 @@ function Accommodations() {
     <DBeigeBackgroundPageWrapper>
       <Title>Accommodations</Title>
       <StyledContainer>
-        <Row>
-          <StyledCol>hi</StyledCol>
-        </Row>
-        <Row>
-          <Col>hi</Col>
-        </Row>
+        {rooms.map((room) => (
+          <StyledRow key={room.id}>
+            <RoomCard>
+              <RoomImage
+                className="room-image"
+                src={RoomPhoto}
+                alt={room.type}
+              />
+              <TextWrapper className="text-wrapper">
+                <RoomTitle className="room-title">{room.type}</RoomTitle>
+                <RoomDescription className="room-description">
+                  {room.description}
+                </RoomDescription>
+              </TextWrapper>
+              <ArrowIcon className="arrow-icon">➔</ArrowIcon>
+            </RoomCard>
+          </StyledRow>
+        ))}
       </StyledContainer>
-      <div>Data: {JSON.stringify(rooms)}</div>
     </DBeigeBackgroundPageWrapper>
   );
 }
