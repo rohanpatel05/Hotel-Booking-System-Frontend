3add94bfb55d95851cb24010152e2ad870333d7b
Rohan Patel <rohangckp@gmail.com>
2024-04-27 14:02:50 -0500

    Created rooms slice and implemented reducers.

---
+++ src/features/rooms/roomsSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: [],
+};
+
+export const roomsSlice = createSlice({
+  name: "rooms",
+  initialState,
+  reducers: {
+    updateRooms: (state, action) => {
+      state.value += action.payload;
+    },
+    reset: (state) => {
+      state.value = [];
+    },
+  },
+});
+
+export const { updateRooms, reset } = roomsSlice.actions;
+
+export default roomsSlice.reducer;
