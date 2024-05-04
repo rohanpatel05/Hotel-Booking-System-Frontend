8f4eea68cbb2aa2bae34f85e4c38a3d9a5729d86
Rohan Patel <rohangckp@gmail.com>
2024-05-03 19:24:37 -0500

    Added booking page to the app.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Home from "./pages/Home.jsx";
 import Room from "./pages/Room/index.jsx";
+import Booking from "./pages/Booking/index.jsx";
 import NotFound from "./pages/NotFound/index.jsx";
 
 const queryClient = new QueryClient();
@@ -17,6 +18,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/room/:roomType" element={<Room />} />
+            <Route exact path="/booking" element={<Booking />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Router>
