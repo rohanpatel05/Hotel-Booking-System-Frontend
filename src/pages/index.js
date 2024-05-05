ea5781fd48c1b72378858017f5494847661a7bdd
Rohan Patel <rohangckp@gmail.com>
2024-05-05 12:01:56 -0500

    Exporting all pages.

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home.jsx";
+export { default as NotFound } from "./NotFound/index.jsx";
+export { default as Room } from "./Room/index.jsx";
+export { default as Booking } from "./Booking/index.jsx";
+export { default as SignUp } from "./SignUp/index.jsx";
