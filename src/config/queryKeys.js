91b04bbdb0a924ffc2c18d3d55aac0d196b322b2
Rohan Patel <rohangckp@gmail.com>
2024-05-11 13:26:29 -0500

    Added react query function keys.

--- src/config/queryKeys.js
+++ src/config/queryKeys.js
@@ -4,3 +4,5 @@ export const GET_ROOMS_QUERY_KEY = "getRooms";
 // User
 export const POST_SIGNUP_QUERY_KEY = "userSignUp";
 export const POST_SIGNIN_QUERY_KEY = "userSignIn";
+export const POST_SIGNOUT_QUERY_KEY = "userSignOut";
+export const POST_REFERSH_QUERY_KEY = "refeshAccessToken";
