89bd09422105060421690adf9d7ee58724a0be1d
Rohan Patel <rohangckp@gmail.com>
2024-05-11 12:02:09 -0500

    Added auth context provider to the app.

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,27 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {Home, Room, Booking, SignUp, SignIn, NotFound} from "./pages"
+import { AuthProvider } from "./contexts/AuthProvider.js"
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <React.Fragment>
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/room/:roomType" element={<Room />} />
-            <Route exact path="/booking" element={<Booking />} />
-            <Route exact path="/signup" element={<SignUp />} />
-            <Route exact path="/signin" element={<SignIn />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </React.Fragment>
+      <AuthProvider>
+        <React.Fragment>
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/room/:roomType" element={<Room />} />
+              <Route exact path="/booking" element={<Booking />} />
+              <Route exact path="/signup" element={<SignUp />} />
+              <Route exact path="/signin" element={<SignIn />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Router>
+        </React.Fragment>
+      </AuthProvider>
     </QueryClientProvider>
   );
 }
