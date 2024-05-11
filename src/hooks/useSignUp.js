dbfd3c11e7deea3afe3c42f4ac102ca12b866844
Rohan Patel <rohangckp@gmail.com>
2024-05-11 12:05:05 -0500

    Created the sign up hook.

---
+++ src/hooks/useSignUp.js
@@ -0,0 +1,30 @@
+import { useMutation } from "@tanstack/react-query";
+import useAuth from "./useAuth.js";
+import { POST_SIGNUP_QUERY_KEY } from "../config/queryKeys.js";
+import { signUpQuery } from "../services/userService";
+
+export const useSignUp = () => {
+  const { signUp } = useAuth();
+
+  const { mutate, isLoading, isError, error } = useMutation({
+    mutationKey: [POST_SIGNUP_QUERY_KEY],
+    mutationFn: signUpQuery,
+    onSuccess: (response) => {
+      signUp({
+        accessToken: response.accessToken,
+        refreshToken: response.refreshToken,
+        user: response.user,
+      });
+    },
+    onError: (error) => {
+      if (error.response) {
+        error.message =
+          error.response.data.message || "Unknown error occurred.";
+      } else {
+        error.message = "Network error occurred!";
+      }
+    },
+  });
+
+  return { mutate, isLoading, isError, error };
+};
