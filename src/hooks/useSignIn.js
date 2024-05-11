77d9c899276b9b0d4c100395c4d7b6bb0d352022
Rohan Patel <rohangckp@gmail.com>
2024-05-11 12:04:56 -0500

    Created the sign in hook.

---
+++ src/hooks/useSignIn.js
@@ -0,0 +1,30 @@
+import { useMutation } from "@tanstack/react-query";
+import useAuth from "./useAuth.js";
+import { POST_SIGNIN_QUERY_KEY } from "../config/queryKeys.js";
+import { signInQuery } from "../services/userService";
+
+export const useSignIn = () => {
+  const { signIn } = useAuth();
+
+  const { mutate, isLoading, isError, error } = useMutation({
+    mutationKey: [POST_SIGNIN_QUERY_KEY],
+    mutationFn: signInQuery,
+    onSuccess: (response) => {
+      signIn({
+        accessToken: response.accessToken,
+        refreshToken: response.refreshToken,
+        user: response.user,
+      });
+    },
+    onError: (error) => {
+      if (error.response) {
+        error.message =
+          error.response.data.message || "Unknown error occurred.";
+      } else {
+        error.message = "Network error occurred!";
+      }
+    },
+  });
+
+  return { mutate, isLoading, isError, error };
+};
