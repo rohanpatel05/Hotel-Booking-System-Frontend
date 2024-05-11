25cd97ba195a07dd378c7533e8a137edd04312cc
Rohan Patel <rohangckp@gmail.com>
2024-05-11 13:27:55 -0500

    Added sign out and refresh access token query functions.

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,12 +2,27 @@ import axiosInstance from "../config/axiosInstance";
 
 export const userUrlEndpoint = "/user";
 
-export const signUp = async (signUpBody) => {
+export const signUpQuery = async (signUpBody) => {
   const { data } = await axiosInstance.post(`/signup`, signUpBody);
   return data;
 };
 
-export const signIn = async (signInBody) => {
+export const signInQuery = async (signInBody) => {
   const { data } = await axiosInstance.post(`/login`, signInBody);
+  return data;
+};
+
+export const signOutQuery = async (accessToken) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+  const { data } = await axiosInstance.post(`/logout`, {}, config);
+  return data;
+};
+
+export const refreshTokenQuery = async (refreshBody) => {
+  const { data } = await axiosInstance.post(`/refresh`, refreshBody);
   return data;
 };
