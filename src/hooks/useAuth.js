1e2a03aaeb7560fe2029e7c7205cb3391a211a4b
Rohan Patel <rohangckp@gmail.com>
2024-05-11 16:10:35 -0500

    Validation added.

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,7 +2,15 @@ import { useContext } from "react";
 import AuthContext from "../contexts/AuthProvider.js";
 
 const useAuth = () => {
-  return useContext(AuthContext);
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error(
+      "AuthContext must be used inside of the AuthContextProvider!"
+    );
+  }
+
+  return context;
 };
 
 export default useAuth;
