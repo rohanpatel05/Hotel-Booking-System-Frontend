02616690dfd03de8e0bb6aba16b62dd5d4dd5f0a
Rohan Patel <rohangckp@gmail.com>
2024-06-01 23:32:07 -0500

    Formatted address being displayed.

--- src/pages/UserInfo/index.jsx
+++ src/pages/UserInfo/index.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { TopBar, OverlayedSpinner as Spinner, UpdateUserInfo, UpdatePassword } from '../../components/index';
 import { Card } from 'react-bootstrap';
@@ -9,6 +8,26 @@ import { NotFound } from '../index';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { GET_USERINFO_QUERY_KEY } from '../../config/queryKeys';
 import { getUserInfo } from '../../services/userService';
+
+const formatAddress = (address) => {
+  const { street, city, state, zipCode } = address;
+  let formattedAddress = "";
+
+  if (street) {
+    formattedAddress += street;
+  }
+  if (city) {
+    formattedAddress += formattedAddress ? `, ${city}` : city;
+  }
+  if (state) {
+    formattedAddress += formattedAddress ? `, ${state}` : state;
+  }
+  if (zipCode) {
+    formattedAddress += formattedAddress ? `, ${zipCode}` : zipCode;
+  }
+
+  return formattedAddress || "No address provided";
+};
 
 function UserInfo() {
   const queryClient = useQueryClient();
@@ -47,7 +66,7 @@ function UserInfo() {
                 <p><strong>Name:</strong> {getUserInfoData.user.name}</p>
                 <p><strong>Email:</strong> {getUserInfoData.user.email}</p>
                 {getUserInfoData.user.phoneNumber ? (<p><strong>Phone Number:</strong> {getUserInfoData.user.phoneNumber}</p>) : null}
-                {getUserInfoData.user.address ? (<p><strong>Address:</strong> {JSON.stringify(getUserInfoData.user.address)}</p>) : null}
+                {getUserInfoData.user.address ? (<p><strong>Address:</strong> {formatAddress(getUserInfoData.user.address)}</p>) : null}
               </>
             ) : getUserInfoIsError ? (<ErrorMessage>Error getting user info: {getUserInfoError.message}</ErrorMessage>) : null}
           </>
